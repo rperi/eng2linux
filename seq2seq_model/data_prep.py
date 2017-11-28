b7f70cf77ab88c08741a6a49e3d1284d9131b40d
inctrl <taejinpa@usc.edu>
2017-11-27 19:32:31 -0800

    data cleaned

--- seq2seq_model/data_prep.py
+++ seq2seq_model/data_prep.py
@@ -1,6 +1,7 @@
 import numpy as np
 import json
 import random
+from augment_data import *
 
 def read_and_store(list_path):
     with open(list_path) as f:
@@ -40,7 +41,20 @@ if __name__ == '__main__':
     train_set = data_out[:int(split_ratio*len(data_out))]
     dev_set = data_out[int(split_ratio*len(data_out)):]
     
-    read_and_write('../data/com-eng_train.txt', train_set)
+    # read_and_write('../data/com-eng_train.txt', train_set)
     read_and_write('../data/com-eng_test.txt', dev_set)
 
+
+    # Parameters
+    # data_file = "../data/com-eng_train.txt"  # file containing the original command-description pairs
+    data_file = "../data/com-eng_train_toy.txt"  # file containing the original command-description pairs
+    out_data_file = "../data/com-eng_train_aug.txt"  # output file where the new augmented pairs are written
+
+    max_num_syn = 3  # maximum number of synonyms per word to be used for augmentation
+
+    FLAG_POS_Stanf = 1  # when set to 0, it uses the averaged perceptron tagger from nltk else uses stanford POS tagger
+    FLAG_Save_Verbs2Syns_dict = 0 # flag to save the verbs-synonyms dictionary to file. Need to manually set path for dictionary
+
+    # Function call
+    augment_eng2linux_Data(data_file, out_data_file, max_num_syn, FLAG_POS_Stanf, FLAG_Save_Verbs2Syns_dict)
 
